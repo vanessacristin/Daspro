4e1d0062e9ab89f7a99ea2f82dc215f2ee6fd3b5
vanessacristin <cristinvanessa6@gmail.com>
2023-10-05 12:47:10 +0700

    percobaan 2

--- PemilihanPercobaan128.java
+++ PemilihanPercobaan128.java
@@ -4,10 +4,10 @@ public class PemilihanPercobaan128 {
         Scanner input28 = new Scanner(System.in);
         System.out.print("Masukkan angka: ");
         int angka = input28.nextInt();
-        if (angka % 2 == 0)
-            System.out.println("Angka "+angka+" bilangan genap");
-        else
-            System.out.println("Angka "+angka+" bilangan ganjil");
+        String hasil;
+        hasil = (angka % 2 == 0 ) ? "Bilangan genap" : "Bilangan ganjil";
+        System.out.println(angka + " adalah " + hasil);
+
         
     }
 }
